4427073c000ef848143735390f0011e02442ff27
Paula <paulasl92@gmail.com>
2016-02-16 13:34:21 -0300

    Mejorado el codigo del archivo js con las funciones jquery

--- choique-2.6.2/web-backend/js/ds-menu.js
+++ choique-2.6.2/web-backend/js/ds-menu.js
@@ -12,20 +12,26 @@
 
 jQuery(document).ready(function() {
 	// inicializacion
-	var conditionalAuthor = 'div.seleccionada #conditionally-author ';
-	var checkAuthor = 'div.seleccionada #pshow-author input:radio[value="author"]';
+        var divFilter = 'div.seleccionada'
+        
+	var conditionalAuthor = divFilter +' #conditionally-author ';
+	var checkAuthor = divFilter +' #pshow-author input:radio[value="author"]';
 	
-	var conditionaDescription = 'div.seleccionada p.conditionally-description';
-	var description = 'div.seleccionada p.description input:checkbox';
+	var conditionaDescription = divFilter + ' p.conditionally-description';
+	var description = divFilter +' p.description input:checkbox';
 	
-	var conditionalFilter = 'div.seleccionada span.conditionally-filter';
-	var checkFilter = 'div.seleccionada p.show-filter';
-        var conditionalTypes = 'div.seleccionada ul.subtipos'
+	var conditionalFilter = divFilter +' span.conditionally-filter';
+	var checkFilter = divFilter +' p.show-filter';
+        var conditionalTypes = divFilter +' ul.subtipos'
 	
-	var conditionalLimit = 'div.seleccionada p.conditionally-limit';
-	var checkLimit = 'div.seleccionada p.limit';
+	var conditionalLimit = divFilter +' p.conditionally-limit';
+	var checkLimit = divFilter +' p.limit';
 
-	
+	var filters = new Array ( {selector:description, conditional:conditionaDescription } ,
+                               {selector:checkFilter, conditional:conditionalFilter},
+                               {selector:checkFilter, conditional:conditionalTypes},
+                               {selector:checkLimit, conditional:conditionalLimit});
+        
 	
         jQuery('#menu-selector div:gt(0)').hide();
         jQuery('#menu-selector ul li:first a').addClass('aqui');
@@ -40,24 +46,17 @@ jQuery(document).ready(function() {
         return false;
          });
          
-         	// binding
+         
+        // binding 
+        jQuery.each( filters, function( i, value ) {
+            jQuery(value.selector).live('change', function() {
+		jQuery(value.conditional).toggle();
+         }); 
+        });
+         
 	jQuery('div.seleccionada #pshow-author input:radio').live('change', function() {
 		jQuery(conditionalAuthor).toggle(jQuery(checkAuthor).is(':checked'));
 	}); 
-	jQuery(description).live('change', function() {
-		jQuery(conditionaDescription).toggle();
-	});
-	jQuery(checkFilter).live('change', function() {
-		jQuery(conditionalFilter).toggle();
-	});
-        jQuery(checkFilter).live('change', function() {
-		jQuery(conditionalTypes).toggle();
-	});
-	jQuery(checkLimit).live('change', function() {
-		jQuery(conditionalLimit).toggle();
-	});
-
-
 });
 
 function justNumbers(e) {
